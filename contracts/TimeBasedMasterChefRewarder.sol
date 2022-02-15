df86bc1875b6aaaf3ad9c6a12384a006345f6f0a
0xSkly <skly.beets@protonmail.com>
2022-02-15 18:00:36 +0100

    add contracts

--- contracts/TimeBasedMasterChefRewarder.sol
+++ contracts/TimeBasedMasterChefRewarder.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "../interfaces/IRewarder.sol";
-import "../token/BeethovenxMasterChef.sol";
+import "./IRewarder.sol";
+import "./BeethovenxMasterChef.sol";
 
 contract TimeBasedMasterChefRewarder is IRewarder, Ownable {
     using SafeERC20 for IERC20;
