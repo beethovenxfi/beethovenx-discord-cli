6cd15548e71f0c09fa529f683ddb3a86c4c63aca
franz <franzns11@gmail.com>
2022-11-03 16:43:15 +0100

    calculate whether relic has reached new level

--- src/crons/update_relic_positions.ts
+++ src/crons/update_relic_positions.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 import { Multicaller } from '../utils/Multicaller';
 import { networkConfig } from '../config/config';
 import reliquaryAbi from '../../abi/Reliquary.json';
+import moment from 'moment';
 
-const reliquarySubgraphUrl: string = 'https://api.thegraph.com/subgraphs/name/beethovenxfi/reliquary/graphql';
+const reliquarySubgraphUrl: string = 'https://api.thegraph.com/subgraphs/name/beethovenxfi/reliquary';
 
 export async function updateRelics() {
     console.log('Schedule updating relics');
@@ -18,34 +19,47 @@ export async function updateLevelsOfRelics() {
     // check ftm wallet balance as well
     // ftmscan api :https://api.ftmscan.com/api?module=account&action=balance&address=0x5A534988535cf27a70e74dFfe299D06486f185B7&apikey=YourApiKeyToken
 
-    const poolLevels = await axios.post<{ pools: [{ pid: Number; levels: [{ level: number }] }] }>(
-        reliquarySubgraphUrl,
-        `query{
-            pools{
-                pid
-                levels{
-                    level
-                }
-            }
-        }`,
-    );
-
-    const poolMaxLevels = poolLevels.data.pools.map((pool) => {
+    const poolLevels = await axios.post<{
+        data: { pools: [{ pid: Number; levels: [{ level: number; requiredMaturity: number }] }] };
+    }>(reliquarySubgraphUrl, {
+        query: `{
+                        pools{
+                            pid
+                            levels{
+                                level
+                                requiredMaturity
+                            }
+                        }
+                    }`,
+    });
+    const poolMaxLevels = poolLevels.data.data.pools.map((pool) => {
         return { pid: pool.pid, maxLevel: Math.max(...pool.levels.map((level) => level.level)) };
     });
 
     const relicIdsToUpdate: number[] = [];
 
-    for (const pool of poolMaxLevels) {
-        const relicsNotInMaxLevel = await axios.post<{ relics: [{ relicId: number }] }>(
-            reliquarySubgraphUrl,
-            `query {   
-                relics(where: {pid: ${pool.pid}, level_lt: ${pool.maxLevel}}){
-                    relicId
-                }
-            }`,
-        );
-        relicsNotInMaxLevel.data.relics.forEach((relic) => relicIdsToUpdate.push(relic.relicId));
+    const now = moment().unix();
+    for (const pool of poolLevels.data.data.pools) {
+        const maxLevel = Math.max(...pool.levels.map((level) => level.level));
+        const relicsNotInMaxLevel = await axios.post<{
+            data: { relics: [{ relicId: number; level: number; entryTimestamp: number }] };
+        }>(reliquarySubgraphUrl, {
+            query: `{   
+                        relics(where: {pid: ${pool.pid}, level_lt: ${maxLevel}}){
+                            relicId
+                            level
+                            entryTimestamp
+                        }
+                    }`,
+        });
+
+        relicsNotInMaxLevel.data.data.relics.forEach((relic) => {
+            const requiredMaturityForNextLevel = pool.levels[relic.level + 1].requiredMaturity;
+            if (relic.entryTimestamp + requiredMaturityForNextLevel < now) {
+                // relic has entered next level
+                relicIdsToUpdate.push(relic.relicId);
+            }
+        });
     }
 
     const multicall = new Multicaller(networkConfig.contractAddresses.multicall, reliquaryAbi);
