5bf767e9dc57567ec568f757be489d33bf42487f
franz <franzns11@gmail.com>
2023-03-26 20:36:35 +0200

    reduce gas

--- src/crons/update_relic_positions.ts
+++ src/crons/update_relic_positions.ts
@@ -120,8 +120,8 @@ async function updateLevelsOfRelics() {
         for (const relicIdToUpdate of relicIdsToUpdate) {
             try {
                 const gasPrice = await proposedGasPriceFantom();
-                if (parseFloat(gasPrice) < 40) {
-                    const txn = await reliquary.updatePosition(relicIdToUpdate, { gasPrice: 40000000000 });
+                if (parseFloat(gasPrice) < 30) {
+                    const txn = await reliquary.updatePosition(relicIdToUpdate, { gasPrice: 30000000000 });
                     await txn.wait();
                     console.log(`Updated relic: ${relicIdToUpdate}.`);
                     updatedRelics++;
