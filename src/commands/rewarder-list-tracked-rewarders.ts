39cc9554d74dd0ad1c584d6cda24dce49669dffc
franz <franzns11@gmail.com>
2023-06-12 16:31:24 +0200

    support multitoken rewarders for list

--- src/commands/rewarder-list-tracked-rewarders.ts
+++ src/commands/rewarder-list-tracked-rewarders.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
+import { SlashCommandBuilder, inlineCode } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
 import { codeBlock } from '@discordjs/builders';
 import fs from 'fs';
@@ -8,8 +8,11 @@ import { CommandHandler } from './index';
 import { ERC20, RewarderMock } from '../../masterchef-types';
 import { ethers } from 'hardhat';
 import { TimeBasedMasterChefRewarder } from '../../masterchef-types/TimeBasedMasterChefRewarder';
+import moment from 'moment';
+import { TimeBasedMasterChefMultiTokenRewarder } from '../../masterchef-types/TimeBasedMasterChefMultiTokenRewarder';
+import { sendMessage, ChannelId } from '../interactions/send-message';
 
-type rewarderOutput = {
+type RewarderOutput = {
     address: string;
     rewardTokenAddress: string;
     rewardTokenSymbol: string;
@@ -25,42 +28,101 @@ async function execute(interaction: CommandInteraction) {
         fs.readFileSync(path.join(__dirname, `../../.rewarder/rewarder-list.json`), 'utf-8'),
     );
 
-    let rewardersOut: rewarderOutput[] = [];
+    let rewardersOut: RewarderOutput[] = [];
     for (const rewarderAddress of rewarders) {
         if (!ethers.utils.isAddress(rewarderAddress)) {
             continue;
         }
-        const rewarder = (await ethers.getContractAt(
-            'TimeBasedMasterChefRewarder',
-            rewarderAddress,
-        )) as unknown as TimeBasedMasterChefRewarder;
-        const masterchefFarmId = await rewarder.masterchefPoolIds(0);
 
-        const rewardTokenAddress = await rewarder.rewardToken();
-        const rewardToken = (await ethers.getContractAt('ERC20', rewardTokenAddress)) as unknown as ERC20;
-        const rewardTokenSymbol = await rewardToken.symbol();
+        let rewarder: RewarderOutput;
 
-        const rewardTokenBalance = await rewardToken.balanceOf(rewarderAddress);
-        const rewardPerSecond = await rewarder.rewardPerSecond();
+        try {
+            rewarder = await getSingleTokenRewarder(rewarderAddress);
+        } catch (e) {
+            // check if it's a multitoken rewarder
+            console.log(`SingleTokenRewarder check failed: ${rewarderAddress}`);
+            try {
+                rewarder = await getMultiTokenRewarder(rewarderAddress);
+            } catch (e) {
+                console.log('error checking for empty rewarders', e);
+                continue;
+            }
+        }
 
-        rewardersOut.push({
+        rewardersOut.push(rewarder);
+
+        await interaction.editReply({
+            content: codeBlock(`Found ${rewardersOut.length} rewarders:`),
+        });
+        for (const rewarder of rewardersOut) {
+            await interaction.followUp({
+                content: codeBlock(`${JSON.stringify(rewarder, null, 2)}`),
+                ephemeral: true,
+            });
+        }
+    }
+}
+
+async function getSingleTokenRewarder(rewarderAddress: string): Promise<RewarderOutput> {
+    const rewarder = (await ethers.getContractAt(
+        'TimeBasedMasterChefRewarder',
+        rewarderAddress,
+    )) as unknown as TimeBasedMasterChefRewarder;
+    const masterchefFarmId = await rewarder.masterchefPoolIds(0);
+
+    const rewardTokenAddress = await rewarder.rewardToken();
+    const rewardToken = (await ethers.getContractAt('ERC20', rewardTokenAddress)) as unknown as ERC20;
+    const rewardTokenSymbol = await rewardToken.symbol();
+
+    const rewardTokenBalance = await rewardToken.balanceOf(rewarderAddress);
+    const rewardPerSecond = await rewarder.rewardPerSecond();
+
+    return {
+        address: rewarderAddress,
+        rewardTokenAddress: rewardTokenAddress,
+        rewardTokenBalance: rewardTokenBalance.toString(),
+        rewardsPerSecond: rewardPerSecond.toString(),
+        rewardTokenSymbol: rewardTokenSymbol,
+        masterchefFarmId: masterchefFarmId.toString(),
+    };
+}
+
+async function getMultiTokenRewarder(rewarderAddress: string): Promise<RewarderOutput> {
+    const rewarder = (await ethers.getContractAt(
+        'TimeBasedMasterChefMultiTokenRewarder',
+        rewarderAddress,
+    )) as TimeBasedMasterChefMultiTokenRewarder;
+
+    const masterchefFarmId = await rewarder.masterchefPoolIds(0);
+
+    const rewarderOutputs: RewarderOutput[] = [];
+
+    const rewardTokens = await rewarder.getRewardTokens();
+    let i = 0;
+    for (const rewardToken of rewardTokens) {
+        const erc20 = (await ethers.getContractAt('ERC20', rewardToken)) as ERC20;
+        const balance = await erc20.balanceOf(rewarder.address);
+        const rewardTokenConfig = await rewarder.rewardTokenConfigs(i);
+
+        const rewardTokenSymbol = await erc20.symbol();
+
+        rewarderOutputs.push({
             address: rewarderAddress,
-            rewardTokenAddress: rewardTokenAddress,
-            rewardTokenBalance: rewardTokenBalance.toString(),
-            rewardsPerSecond: rewardPerSecond.toString(),
-            rewardTokenSymbol: rewardTokenSymbol,
             masterchefFarmId: masterchefFarmId.toString(),
+            rewardsPerSecond: rewardTokenConfig.rewardsPerSecond.toString(),
+            rewardTokenAddress: rewardToken,
+            rewardTokenBalance: balance.toString(),
+            rewardTokenSymbol: rewardTokenSymbol,
         });
     }
-    await interaction.editReply({
-        content: codeBlock(`Found ${rewardersOut.length} rewarders:`),
-    });
-    for (const rewarder of rewardersOut) {
-        await interaction.followUp({
-            content: codeBlock(`${JSON.stringify(rewarder, null, 2)}`),
-            ephemeral: true,
-        });
-    }
+    return {
+        address: rewarderAddress,
+        masterchefFarmId: masterchefFarmId.toString(),
+        rewardTokenAddress: rewarderOutputs.map((tokens) => tokens.rewardTokenAddress).join(','),
+        rewardTokenBalance: rewarderOutputs.map((tokens) => tokens.rewardTokenBalance).join(','),
+        rewardsPerSecond: rewarderOutputs.map((tokens) => tokens.rewardsPerSecond).join(','),
+        rewardTokenSymbol: rewarderOutputs.map((tokens) => tokens.rewardTokenSymbol).join(','),
+    };
 }
 
 export const rewarderListTracked: CommandHandler = {
