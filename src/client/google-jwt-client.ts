9830b391d3285285c424e01c903131f7cb524a79
groninge <groninge@gmail.com>
2024-01-05 12:13:45 +0100

    add client

---
+++ src/client/google-jwt-client.ts
@@ -0,0 +1,21 @@
+import { google } from 'googleapis';
+import { JWT } from 'google-auth-library';
+
+export class GoogleJwtClient {
+    public async getAuthorizedSheetsClient(user: string, privateKey: string): Promise<JWT> {
+        const jwtClient = new google.auth.JWT(
+            user,
+            undefined,
+            privateKey,
+            'https://www.googleapis.com/auth/spreadsheets',
+        );
+        await jwtClient.authorize(function (err, result) {
+            if (err) {
+                console.log(`Error authorizing google jwt client: ${err}`);
+            }
+        });
+        return jwtClient;
+    }
+}
+
+export const googleJwtClient = new GoogleJwtClient();
