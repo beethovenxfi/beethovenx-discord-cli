ec692cb541660f28265ea4c21789c849766e7865
franz <franzns11@gmail.com>
2024-01-08 13:20:13 +0100

    fix private key retrieval

--- src/crons/send_op_farms_reminder.ts
+++ src/crons/send_op_farms_reminder.ts
@@ -80,7 +80,7 @@ export async function opFarmsReminder(): Promise<void> {
     try {
         jwtClient = await googleJwtClient.getAuthorizedSheetsClient(
             process.env.GOOGLE_CLIENT_EMAIL!,
-            process.env.GOOGLE_CLIENT_PRIVATE_KEY!,
+            process.env.GOOGLE_CLIENT_PRIVATE_KEY!.split(String.raw`\n`).join('\n'),
         );
     } catch (e) {
         console.log(`Could not get google sheet credentials. Error: ${e}`);
