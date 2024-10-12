8842d15a80578ccd97dd7170d409ed7c64dcae29
franz <franzns11@gmail.com>
2024-10-12 20:17:59 +0200

    only claim when gas is cheap

--- src/crons/claim_sftmx_rewards.ts
+++ src/crons/claim_sftmx_rewards.ts
@@ -1,4 +1,10 @@
+import { BigNumber } from 'ethers';
+import { parseFixed } from '@ethersproject/bignumber';
 import ftmstakingAbi from '../../abi/FTMStaking.json';
+import { proposedGasPriceFantom } from '../utils';
+import { ChannelId, sendMessage } from '../interactions/send-message';
+import { networkConfig } from '../config/config';
+import { inlineCode } from '@discordjs/builders';
 const { ethers } = require('hardhat');
 
 const ftmStakingContract = '0xB458BfC855ab504a8a327720FcEF98886065529b';
@@ -13,6 +19,21 @@ export async function claimSftmxRewards() {
 async function claimAllSftmxRewards() {
     console.log('claiming sftmx rewards');
     const sftmx = await ethers.getContractAt(ftmstakingAbi, ftmStakingContract);
-    const txn = await sftmx.claimRewardsAll();
-    await txn.wait();
+
+    const updaterBalance: BigNumber = await ethers.provider.getBalance(networkConfig.walletAddresses.relicUpdater);
+    if (updaterBalance.lt(parseFixed(`1`, 18))) {
+        return;
+    }
+
+    const gasPrice = await proposedGasPriceFantom();
+    try {
+        if (parseFloat(gasPrice) < 100) {
+            const txn = await sftmx.claimRewardsAll({
+                gasPrice: parseFloat(gasPrice) * 5968460,
+            });
+            await txn.wait();
+        }
+    } catch (e) {
+        await sendMessage(ChannelId.SERVER_STATUS, `Did not claim sftmx rewards, gas price too high: ${gasPrice}!`);
+    }
 }
